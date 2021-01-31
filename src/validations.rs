be9a93fd8a73c5198b87b520d882fc5e5d222e1c
Marco Yuen <marcoy@gmail.com>
2021-01-30 23:19:57 -0500

    validate generate_integer_sequences

--- src/validations.rs
+++ src/validations.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
 use valid::{Validate, ValidationResult};
-use valid::constraint::Bound;
+use valid::constraint::{Bound, Length};
 
+use crate::random_org_constraint::EveryElement;
 use crate::SeqBound;
-use crate::random_org_constraint::SameVariant;
 
 fn to_result<C, T>(validation_res: ValidationResult<C, T>) -> Result<T> {
   validation_res
@@ -57,11 +57,48 @@ pub fn generate_uuids(n: u16) -> Result<u16> {
   to_result(validation_res)
 }
 
-pub fn generate_integer_sequences(n: u16, _length: SeqBound, min: SeqBound, max: SeqBound) -> Result<(u16, u16, SeqBound, SeqBound)> {
+pub fn generate_integer_sequences(n: u16, length: SeqBound, min: SeqBound, max: SeqBound) -> Result<(u16, SeqBound, SeqBound, SeqBound)> {
   let n_bound = Bound::ClosedRange(1, 1000);
+  let len_bound = Bound::ClosedRange(1, 10_000);
+  let min_max_bound = Bound::ClosedRange(-1_000_000_000, 1_000_000_000);
 
-  n.validate("n", &n_bound)
-    .and((min, max).validate(("min", "max"), &SameVariant));
+  match (length, min, max) {
+    (SeqBound::Uniform(length), SeqBound::Uniform(min), SeqBound::Uniform(max)) => {
+      let validation_res: ValidationResult<(), (u16, SeqBound, SeqBound, SeqBound)> = n.validate("n", &n_bound)
+        .and(length.validate("length", &len_bound))
+        .and(min.validate("min", &min_max_bound))
+        .and(max.validate("max", &min_max_bound))
+        .map(|(((n, length), min), max)| (n, SeqBound::Uniform(length), SeqBound::Uniform(min), SeqBound::Uniform(max)))
+        .result();
+      to_result(validation_res)
+    }
+    (SeqBound::Multiform(length), SeqBound::Multiform(min), SeqBound::Multiform(max)) => {
+      let validation_res: ValidationResult<(), (u16, SeqBound, SeqBound, SeqBound)> = n.validate("n", &n_bound)
+        .and_then(|n| {
+          let exact_len = Length::Exact(n as usize);
+          let every = EveryElement(len_bound.clone());
+          length.validate("length", &exact_len)
+            .and_then(|ls| ls.validate("length", &every))
+            .combine(n)
+        })
+        .and_then(|(n, length)| {
+          let exact_len = Length::Exact(n as usize);
+          let every = EveryElement(min_max_bound.clone());
+          min.validate("min", &exact_len)
+            .and_then(|ls| ls.validate("min", &every))
+            .combine((n, length))
+        })
+        .and_then(|((n, length), min)| {
+          let exact_len = Length::Exact(n as usize);
+          let every = EveryElement(min_max_bound.clone());
+          max.validate("max", &exact_len)
+            .and_then(|ls| ls.validate("max", &every))
+            .combine((n, length, min))
+        })
+        .map(|((n, length, min), max)| (n, SeqBound::Multiform(length), SeqBound::Multiform(min), SeqBound::Multiform(max))).result();
 
-  unimplemented!()
+      to_result(validation_res)
+    }
+    _ => Err(anyhow::Error::msg("Mismatch parameter variants"))
+  }
 }
